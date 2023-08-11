73f90063f05bbfbe8689c624281c377398646b30
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-08-12 00:52:23 +0100

    GetMapping, DeleteMapping and PostMapping

--- src/main/java/com/nokia/erraoudi/photoz/clone/Photo.java
+++ src/main/java/com/nokia/erraoudi/photoz/clone/Photo.java
@@ -1,4 +1,36 @@
 package com.nokia.erraoudi.photoz.clone;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class Photo {
+    private String id;
+    @NotEmpty
+    private String fileName;
+
+    public Photo() {
+    }
+
+    public Photo(String id, String fileName) {
+        this.id = id;
+        this.fileName = fileName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 }
+
+
+
